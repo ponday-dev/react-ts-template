69222f3e07f50cc7875bd31d3bd98907f77bfa4c
ponday-dev <ponday.dev+github@gmail.com>
2019-02-15 01:51:21 +0900

    refactor: 💡 split codes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.scss";
 
-const App = () => <div>Hello, World</div>;
+import { App } from "./App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
